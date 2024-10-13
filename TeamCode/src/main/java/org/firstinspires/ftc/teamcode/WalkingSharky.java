c43bbe8980f0048ace386548cca02ab1c5618705
Dellrobo4 <techalongs@gmail.com>
2024-10-13 13:53:53 -0700

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WalkingSharky.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Walking Sharky")
+public class WalkingSharky extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        Robot sharky = new Robot(hardwareMap, telemetry, this, 0.9);
+        sharky.init();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (opModeIsActive()) {
+                // Drivetrain controls
+                double y = 0;
+
+                if (gamepad1.right_bumper) sharky.drive(0.5, gamepad1, y); // Standard - 0.84
+                else if (gamepad1.left_bumper) sharky.drive(0.2, gamepad1, y); // Standard - 0.4
+                else sharky.drive(0.4, gamepad1, y);
+                // Set normal speed to 0.5 at beginning of next season - for practice
+
+                sharky.displayData();
+            }
+        }
+    }
+}
