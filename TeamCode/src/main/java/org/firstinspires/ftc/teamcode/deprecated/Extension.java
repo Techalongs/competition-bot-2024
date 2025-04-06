ca7a04296b83b94b43ce968bff9d243f35dbde1e
Dave <dave@davemx.com>
2025-04-05 22:14:02 -0700

    Upgrading FTCLib to SolversLib.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deprecated/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deprecated/Extension.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.arcrobotics.ftclib.controller.PIDFController;
+import com.seattlesolvers.solverslib.controller.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
